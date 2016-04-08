2682a79dfddd90bb5225bf67bdd9fae4daaeabe3
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2016-04-08 09:22:26 +0100

    Add script to send email to single address

---
+++ email/sendEmailUsingToField.js
@@ -0,0 +1,53 @@
+var nodemailer = require('nodemailer');
+var parse = require('csv-parse');
+var fs = require('fs');
+var nconf = require('nconf');
+
+// Load csv file and credentials
+nconf.argv({
+    "e": {
+      alias: 'email',
+      describe: 'JSON describing email to send',
+      demand: true
+    }
+}).file({ file: './config.json'} );
+var user = nconf.get('gmail_user');
+var pass = nconf.get('gmail_password');
+var emailPath = nconf.get('email');
+
+// Load email object
+var emailJSON = fs.readFileSync(emailPath, 'utf8');
+var email = JSON.parse(emailJSON);
+
+sendEmail();
+
+function sendEmail() {
+	// create reusable transporter object using SMTP transport
+	var transporter = nodemailer.createTransport({
+	    service: "Gmail",
+            maxMessages: Infinity,
+	    auth: {
+		user: user,
+		pass: pass
+	    }
+	});
+
+	// setup e-mail data
+	var mailOptions = {
+	    from: email.from,
+	    to: email.to,
+	    subject: email.subject,
+	    html: fs.readFileSync(email.body, 'utf8')
+	};
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, function(error, info){
+	    if(error){
+		return console.log(error);
+	    } else {
+		    console.log('Message sent: ' + info.response);
+		    console.log('from:' + user);
+		    console.log('email.to: ' + email.to);
+	    }
+	});
+}
